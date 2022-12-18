7c97d8e503ba403e8204af03f2a7995701de6de4
bh9232 <bhong9232@gmail.com>
2022-12-18 13:22:56 -0500

    still sutck on no data update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const App = () => {
         <Route exact path="/campuses" component={AllCampusesContainer} />
         <Route exact path="/newcampus" component={NewCampusContainer} />
         <Route exact path="/campus/:id" component={CampusContainer} />
+        {/* <Route exact path="/editcampus/:id" component={EditCampusContainer} /> */}
         <Route exact path="/students" component={AllStudentsContainer} />
         <Route exact path="/newstudent" component={NewStudentContainer} />
         <Route exact path="/editstudent/:id" component={EditStudentContainer} />
