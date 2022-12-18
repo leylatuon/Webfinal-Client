74bcdd37f7806982e48d60cf9084eb354b90fc25
bh9232 <bhong9232@gmail.com>
2022-12-17 21:28:03 -0500

    forgot to git add

---
+++ src/components/views/EditStudentView.js
@@ -0,0 +1,90 @@
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+// Create styling for the input form
+const useStyles = makeStyles( () => ({
+  formContainer:{  
+    width: '500px',
+    backgroundColor: '#f0f0f5',
+    borderRadius: '5px',
+    margin: 'auto',
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    textDecoration: 'none'
+  }, 
+  customizeAppBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
+  },
+  formTitle:{
+    backgroundColor:'#c5c8d6',
+    marginBottom: '15px',
+    textAlign: 'center',
+    borderRadius: '5px 5px 0px 0px',
+    padding: '3px'
+  },
+}));
+
+const EditStudentView = (props) => {
+  const {student, handleChange, handleSubmit } = props;
+  const classes = useStyles();
+
+  // Render a New Student view with an input form
+  return (
+    <div>
+      <h1>Edit Student</h1>
+
+      <div className={classes.root}>
+        <div className={classes.formContainer}>
+          <div className={classes.formTitle}>
+            <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
+              Edit Student
+            </Typography>
+          </div>
+          <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
+            <label style= {{color:'#11153e', fontWeight: 'bold'}}>First Name: </label>
+            <input type="text" name="firstname" placeholder={student.firstname} onChange ={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Last Name: </label>
+            <input type="text" name="lastname" placeholder={student.lastname} onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Campus Id: </label>
+            <input type="text" name="campusId" placeholder={student.campusId} onChange={(e) => handleChange(e)} /> 
+            <br/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Email Address: </label>
+            <input type="text" name="email" placeholder={student.email} onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>GPA: </label>
+            <input type="number" step="0.01" name="gpa" placeholder={student.gpa} onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Image URL: </label>
+            <input type="text" name="imageURL" placeholder={student.imageURL} onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <Button variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+            <br/>
+            <br/>
+          </form>
+          </div>
+      </div>
+    </div>    
+  )
+}
+
+export default EditStudentView;
